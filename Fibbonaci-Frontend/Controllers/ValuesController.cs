f9c0130d991952863dd21c19f2a217db0e9f5fe9
Kai Jellinghaus <herrucan.schmitt@gmail.com>
2019-05-06 18:43:21 +0200

    Use Content

--- Fibbonaci-Frontend/Controllers/ValuesController.cs
+++ Fibbonaci-Frontend/Controllers/ValuesController.cs
@@ -10,6 +10,7 @@ namespace Fibbonaci_Frontend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class ValuesController : ControllerBase
     {
         private IBus _bus;
@@ -21,11 +22,11 @@ namespace Fibbonaci_Frontend.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<int>> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
             if (id < 0)
                 return NotFound(id);
-            return Ok((await _bus.RequestAsync<FibbonaciRequest, FibbonaciResponse>(new FibbonaciRequest(id))).Value);
+            return Content((await _bus.RequestAsync<FibbonaciRequest, FibbonaciResponse>(new FibbonaciRequest(id))).Value.ToString());
         }
     }
 }
